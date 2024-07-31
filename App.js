982a79c21f4b896267b3d25af378b56bec63c1a9
fadhil_octo <mochammad.octokurniawan@indocyber.id>
2024-07-31 09:47:46 +0700

    install react native paper

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { View, Text, SafeAreaView, StatusBar } from "react-native";
 import React from "react";
 import Routes from "./src/routes";
+import { PaperProvider } from "react-native-paper";
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Routes />
+      <PaperProvider>
+        <Routes />
+      </PaperProvider>
     </SafeAreaView>
   );
 }
--- src/views/screen/home/HomeScreen.js
+++ src/views/screen/home/HomeScreen.js
@@ -1,14 +1,23 @@
-import { View, Text, Button } from "react-native";
+import { View, Text } from "react-native";
+import { Button, TextInput } from "react-native-paper";
 import React from "react";
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        gap: 20,
+        marginRight: 10,
+        marginLeft: 10,
+      }}
+    >
       <Text>Home Screen</Text>
-      <Button
-        onPress={() => navigation.navigate("Details")}
-        title="Go To Details"
-      />
+      <TextInput label="Email" />
+      <Button onPress={() => navigation.navigate("Details")} mode="contained">
+        Button Paper
+      </Button>
     </View>
   );
 }
